c4ffdc644be111eec64104f371b5c81747fa5e9d
TomerAsraf <tomerasraf2@gmail.com>
2021-07-27 21:46:43 +0300

    V0.1.2
    Cleaner Bug fix
    Editing level Design

--- Assets/Scripts/SceneMangement/Desroyer.cs
+++ Assets/Scripts/SceneMangement/Desroyer.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class Desroyer : MonoBehaviour
 {
+    Transform playerPosition = null;
+
+    private float offsetZ = -100f;
+
+    private void Awake()
+    {
+        playerPosition = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        Vector3 offSetPosition = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, 2.8f, 10f), offsetZ + playerPosition.position.z);
+        transform.position = offSetPosition;
+
+
+
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
         Destroy(other.gameObject);
     }
 
-    
+
 }
